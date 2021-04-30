beabc29f7e653c1e9ad3c3c51ab2ad6a2b34605d
jian zhiqiang <2631095834@qq.com>
2021-04-30 09:58:57 +0800

    分支测试

--- src/main/java/com/jzq/Application.java
+++ src/main/java/com/jzq/Application.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/3/12 23:12
  */
 @SpringBootApplication(scanBasePackages = "com.jzq")
-//@MapperScan(basePackages = "com.jzq.service")
+//@MapperScan(basePackages = "com.jzq.service")123
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
